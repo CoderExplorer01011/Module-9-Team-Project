c076f92ad59b9631b8a3d785b5cd687f4835db69
captainjames007 <wahidjamie2@gmail.com>
2024-11-24 16:42:18 -0600

    Update some_ python_program.py

--- some_ python_program.py
+++ some_ python_program.py
@@ -71,3 +71,36 @@ button_0 =  Button(root, text="0", padx=40, pady=20, command=lambda: button_clic
 button_add =  Button(root, text="+", padx=39, pady=20, command=lambda: button_operator("+"))  # Creates a button labeled "+" and links it to the button_operator function
 button_equal =  Button(root, text="   =   ", padx=79, pady=20, command=button_equal)  # Creates a button labeled "=" and links it to the button_equal function
 button_clear =  Button(root, text="Clear", padx=79, pady=20, command=button_clear)  # Creates a button labeled "Clear" and links it to the button_clear function
+# Document what the following lines of code do here
+
+# See the description of a Lambda function above
+button_subtract =  Button(root, text="-", padx=40, pady=20, command=lambda: button_operator("-"))
+button_multiply =  Button(root, text="*", padx=40, pady=20, command=lambda: button_operator("*"))
+button_divide =  Button(root, text="/", padx=40, pady=20, command=lambda: button_operator("/"))
+
+# Document what the following lines of code do here
+
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+
+button_0.grid(row=4, column=0)
+button_add.grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)
+button_clear.grid(row=4, column=1, columnspan=2)
+
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
+
+# Document what the following line of code do here
+
+root.mainloop()
